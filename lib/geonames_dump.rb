1fe6d94bcc973b4cdc5a9080809ddb5e2a7c8310
Thomas Kienlen <kommander@laposte.net>
2013-11-26 09:40:36 +0100

    GeonamesDump.search :auto --> city, alternate name, admin1, admin2, country, feature

--- lib/geonames_dump.rb
+++ lib/geonames_dump.rb
@@ -9,10 +9,26 @@ module GeonamesDump
   def self.search(query, options = {})
     ret = nil
 
-    type = options[:type] || :city
+    type = options[:type] || :auto
     begin
-      model = "geonames_#{type.to_s}".camelcase.constantize
-      ret = model.search(query)
+      case type
+      when :auto
+        # city name
+        ret = GeonamesCity.search(query)
+        # alternate name
+        ret = GeonamesAlternateName.search(query) if ret.blank?
+        # admin1
+        ret = GeonamesAdmin1.search(query) if ret.blank?
+        # admin2
+        ret = GeonamesAdmin2.search(query) if ret.blank?
+        # country
+        ret = GeonamesCountry.search(query) if ret.blank?
+        # feature
+        ret = GeonamesFeature.search(query) if ret.blank?
+      else
+        model = "geonames_#{type.to_s}".camelcase.constantize
+        ret = model.search(query)
+      end
     rescue NameError => e
       Rails.logger.error "Unknown type for GeonamesDump : #{e}"
       #raise
